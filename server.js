a2703e34e583e5e01063f0ed459808356449cb2d
Josh Loya <jloya3@oreillyauto.com>
2018-04-02 15:11:48 -0400

    Always redirect to https in production

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var serveStatic = require('serve-static');
 var port = process.env.PORT || 8080;
 
 app = express();
+if (process.env.NODE_ENV === 'production') {
+  var enforce = require('express-sslify');
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 app.use(serveStatic(path.join(__dirname, '/www')));
 
 app.listen(port, () => console.log('server listening on port ' + port));
