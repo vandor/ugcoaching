a8ae5827ff15dc7a0878998095a2c9b99ff094ff
Josh Loya <jloya3@oreillyauto.com>
2018-04-09 12:06:32 -0500

    Add middleware so express hands down requests to the framework7 router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var history = require('connect-history-api-fallback');
 var port = process.env.PORT || 8080;
 
 app = express();
@@ -8,6 +9,10 @@ if (process.env.NODE_ENV === 'production') {
   var enforce = require('express-sslify');
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
-app.use(serveStatic(path.join(__dirname, '/www')));
+const staticMiddleware = serveStatic(path.join(__dirname, '/www'));
+app.use(staticMiddleware);
+app.use(history({}));
+app.use(staticMiddleware);
+
 
 app.listen(port, () => console.log('server listening on port ' + port));
