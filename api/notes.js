573dc2e867695390564c84688fabc35352759163
Josh Loya <jloya3@oreillyauto.com>
2018-04-10 16:58:40 -0500

    Respond to api call with json

--- api/notes.js
+++ api/notes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 
 router.get('/', (req, res) => {
-  res.status(200).send('here are your notes');
+  res.status(200).json({"message": "Here are your notes!"});
 });
 
 module.exports = router;
